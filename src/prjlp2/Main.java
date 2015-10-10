867b8a74aeef6c22f26ad37a83b2a548fb35a511
Ademir <ademirbezerra2012@gmail.com>
2015-10-10 01:02:16 -0300

    Bibliotecas adicionadas

--- src/prjlp2/Main.java
+++ src/prjlp2/Main.java
@@ -17,7 +17,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic hereasdasdasdcasdasdasdas
         
-        System.out.println("asdasdas");
     }
     
 }
